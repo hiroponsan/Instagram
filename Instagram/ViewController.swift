56f0cc6c2bcc20d01c01b4015e6b34b9e886b40d
hiroponsan <Chuo-univTanaka@outlook.jp>
2016-03-20 15:58:12 +0900

    added view controller

--- Instagram/ViewController.swift
+++ Instagram/ViewController.swift
@@ -1,14 +1,21 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by 田中　博成 on 2016/03/20.
+//  Copyright © 2016年 hiropon. All rights reserved.
+//
+
 import UIKit
 import ESTabBarController
-
 class ViewController: UIViewController {
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        // Do any additional setup after loading the view, typically from a nib.
         setupTab()
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -45,4 +52,7 @@ class ViewController: UIViewController {
             self.presentViewController(imageViewController!, animated: true, completion: nil)
             }, atIndex: 1)
     }
+
+
 }
+
